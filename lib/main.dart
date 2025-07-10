87a81e1a630639d917441f0a931156881774ceb0
Lans12345 <olanalans12345@gmail.com>
2025-07-10 08:39:27 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:kaffi_cafe/firebase_options.dart';
 import 'package:kaffi_cafe/screens/auth/login_screen.dart';
 import 'package:kaffi_cafe/screens/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'kaffi-cafe',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
